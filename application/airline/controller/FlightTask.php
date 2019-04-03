4cf0d45cfbc479a8a5fbf30f5ccd1f90fd3946a1
Smile佳 <522246447@qq.com>
2019-04-03 15:49:31 +0800

    add thinphp

--- application/airline/controller/FlightTask.php
+++ application/airline/controller/FlightTask.php
@@ -14,6 +14,7 @@ class FlightTask extends Controller
     public function index(){
         $flights=Db::table("flight_base")->whereTime('end_date','>',date("Y-m-d",time()))->select();
         
+
         dump($flights);
         
         return view();
@@ -27,5 +28,61 @@ class FlightTask extends Controller
 
     public function flightTaskDetails(){
         return view();
+    }
+
+    public function ajaxTest(Request $request){
+        #接收前端的ajax信息，写入文件测试
+        // $myfile = fopen("ajaxtest.txt", "w") or die("Unable to open file!");
+        // $finalData= $request->param("final_data");
+        // fwrite($myfile, $finalData);
+        // fclose($myfile);
+
+        $finalData="{\"occupation\":\"F/O\",\"PPN\":\"432423\",\"DOE\":\"2019-04-10\",\"POB\":\"天津\",\"name\":\"罗佳\",\"gender\":\"MALE\",\"nationality\":\"中国\",\"DOB\":\"2019-04-10\"}";
+
+        $msg;//返回给前端的信息
+
+        #从session读取companyId
+        $companyId=Session::get("airline_companyId");
+
+        #判断用户是否登录
+        $is_login=boolval($companyId);
+        if(!$is_login){
+            $msg=[
+                "statu"=>"error",
+                "detailes"=>"请先登录"
+            ];
+        }
+
+        #解析前端传送过来的数据
+        $staff=json_decode($finalData,true);
+
+        #生成新的员工id
+        $staffInfo=Db::table("flight_leg_crew_staff")->where(["leg_match_code"=>"123"])->order('staff_id','desc')->limit(1)->select();
+        if(empty($staffId)){
+            $staffId=1;
+        }
+        else{
+            $staffId=$staffInfo[0]["staff_id"]+1;
+        }
+
+        #组装数据
+        $staff["staff_id"]=$staffId;
+
+        #提交数据到数据库
+        $count=Db::table("flight_leg_crew_staff")->insert($staff);
+        if($count>0){
+            $msg=[
+                "statu"=>"success",
+                "detailes"=>"提交成功"
+            ];
+        }else{
+            $msg=[
+                "statu"=>"error",
+                "detailes"=>"数据存入数据库异常"
+            ];
+        }
+
+        dump($staff);
+        dump($msg);
     }
 }
